cfd04a5d7e19870693d2ffc7d27cc495ea09d72c
xx-3-xx <lowliana0524@gmail.com>
2025-05-18 00:41:48 +0800

    globe

--- inventory-assistant-frontend/src/app/pages/globe/globe.component.ts
+++ inventory-assistant-frontend/src/app/pages/globe/globe.component.ts
@@ -1,100 +1,73 @@
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import Globe from 'globe.gl';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-globe',
   templateUrl: './globe.component.html',
   styleUrls: ['./globe.component.css']
 })
+
 export class GlobeComponent implements AfterViewInit {
   @ViewChild('globeContainer', { static: true }) globeContainer!: ElementRef;
+  world: any;
+
+  locations = [
+    { name: 'Astoria', lat: 40.7644, lng: -73.9235, details: 'Astoria details...' },
+    { name: 'Lower Manhattan', lat: 60.7075, lng: 30.0113, details: 'Lower Manhattan details...' },
+    { name: 'Hell\'s Kitchen', lat: 0.7638, lng: -73.9918, details: 'Hell\'s Kitchen details...' }
+  ];
+
+      //   //       .bumpImageUrl('https://unpkg.com/three-globe/example/img/earth-topology.png')
+      //         .showAtmosphere(true)
+      // .atmosphereColor('lightskyblue')
+      // .atmosphereAltitude(0.25)
+
 
   ngAfterViewInit(): void {
-   const world = new Globe(this.globeContainer.nativeElement)
-    .globeImageUrl('https://unpkg.com/three-globe/example/img/earth-dark.jpg')
-    .bumpImageUrl('https://unpkg.com/three-globe/example/img/earth-topology.png')
-    .backgroundImageUrl('https://unpkg.com/three-globe/example/img/night-sky.png')
-    .showAtmosphere(true)
-    .atmosphereColor('lightskyblue')
-    .atmosphereAltitude(0.25);
+    this.world = new Globe(this.globeContainer.nativeElement)
+      .globeImageUrl('https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
+      .bumpImageUrl('https://www.solarsystemscope.com/textures/download/8k_earth_normal_map.jpg')
+      .backgroundImageUrl('https://unpkg.com/three-globe/example/img/night-sky.png')
+      .showAtmosphere(true)
+      .atmosphereColor('#a0d8ef')  
+      .atmosphereAltitude(0.35)    
+      .pointsData(this.locations)
+      .pointLat('lat')
+      .pointLng('lng')
+      .pointAltitude(0.07)
+      .pointRadius(1.0)
+      .pointColor(() => 'white')
+      .pointLabel((d: any) => `
+        <div class="point-label">
+          <div class="details">
+            <b>${d.name}</b><br>
+            ${d.details}
+          </div>
+        </div>
+      `)
+      .customThreeObject(() => {
+        const ring = new THREE.Mesh(
+          new THREE.RingGeometry(0.1, 0.12, 32),
+          new THREE.MeshBasicMaterial({
+            color: 0x00ffff,
+            side: THREE.DoubleSide,
+            transparent: true,
+            opacity: 0.5
+          })
+        );
+        ring.rotation.x = -Math.PI / 2;
+        return ring;
+      });
 
+    this.world.controls().autoRotate = true;
+    this.world.controls().autoRotateSpeed = 0.5;
+  }
 
-    // Enable auto-rotation
-    world.controls().autoRotate = true;
-    world.controls().autoRotateSpeed = 0.5;
-
-    // Additional configurations like markers or arcs can be added here
+  focusOnLocation(name: string): void {
+    const location = this.locations.find(l => l.name === name);
+    if (location) {
+      this.world.pointOfView({ lat: location.lat, lng: location.lng, altitude: 1.5 }, 1500);
+    }
   }
 }
-
-// import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-// import { NzModalService } from 'ng-zorro-antd/modal';
-// import Globe from 'globe.gl';
-// import {Store} from '../../../models/store.model';
-// import {StoreService} from '../../../services/store.service';
-
-// @Component({
-//   selector: 'app-globe',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './globe.component.html',
-//   styleUrl: './globe.component.css'
-// })
-// export class GlobeComponent implements OnInit {
-
-//   // @ViewChild('globeContainer', { static: true }) globeContainer!: ElementRef;
-//   //
-//   // stores: Store[] = [];
-//   // globe: any;
-//   // selectedStore: Store | null = null;
-//   //
-//   // constructor(
-//   //   private storeService: StoreService,
-//   //   private modalService: NzModalService
-//   // ) {}
-//   //
-//   ngOnInit(): void {
-//   //  this.loadStores();
-//   }
-//   //
-//   // loadStores(): void {
-//   //   this.storeService.getStores().subscribe(stores => {
-//   //     this.stores = stores;
-//   //     this.initGlobe();
-//   //   });
-//   // }
-//   //
-//   // initGlobe(): void {
-//   //   // Initialize the globe - pass the element to the constructor
-//   //   this.globe = new Globe(this.globeContainer.nativeElement)
-//   //     .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
-//   //     .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
-//   //     .pointsData(this.stores)
-//   //     .pointLat('lat')
-//   //     .pointLng('lng')
-//   //     .pointColor(() => 'red') // Red pins
-//   //     .pointAltitude(0.1) // Slightly elevated pins
-//   //     .pointRadius(0.5) // Size of pins
-//   //     .pointsMerge(true)
-//   //
-//   //
-//   //   // Set initial view angle
-//   //   this.globe.pointOfView({ lat: 25, lng: 0, altitude: 2.5 });
-//   // }
-//   //
-//   // showStoreDetails(store: Store): void {
-//   //   this.selectedStore = store;
-//   //
-//   //   // Focus the globe on the selected store
-//   //   this.globe.pointOfView({
-//   //     lat: store.lat,
-//   //     lng: store.lng,
-//   //     altitude: 1.5
-//   //   }, 1000); // 1000ms animation duration
-//   // }
-//   //
-//   // closeDetails(): void {
-//   //   this.selectedStore = null;
-//   // }
-
-//  }
