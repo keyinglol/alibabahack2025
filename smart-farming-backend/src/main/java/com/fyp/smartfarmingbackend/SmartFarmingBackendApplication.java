c545ef238f74fce958ef1ae7c51fe1f03faddc20
keyinglol <Keyingliew@gmail.com>
2025-05-17 11:44:46 +0800

    Add frontend as part of the main repo

---
+++ smart-farming-backend/src/main/java/com/fyp/smartfarmingbackend/SmartFarmingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.smartfarmingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFarmingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFarmingBackendApplication.class, args);
+    }
+
+}
